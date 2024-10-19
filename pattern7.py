9bcdb05f5385dd60290b9e4afc8fcc5a9624a6bb
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2024-10-19 23:38:45 +0530

    lec2

---
+++ pattern7.py
@@ -0,0 +1,16 @@
+def nStarTriangle(n: int) -> None:
+    for i in range(0,n):
+        for j in range(0,n-i-1):
+            print(" ",end="")
+        for j in range(0,2*i+1):
+            print("*",end="")
+        for j in range(0,n-i-1):
+            print(" ",end="")
+        print()
+
+
+#     *    
+#    ***   
+#   *****  
+#  ******* 
+# *********
